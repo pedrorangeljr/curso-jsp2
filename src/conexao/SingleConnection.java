e4e29a7a0db8479412a64bc6f511cd555296c5e9
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2024-03-26 14:48:17 -0300

    classe SingleConnection

--- src/conexao/SingleConnection.java
+++ src/conexao/SingleConnection.java
@@ -1,5 +1,48 @@
 package conexao;
 
-public class SingleConnection {
+/*Classe de conexão com banco de dados*/
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnection {
+	
+	private static String url = "jdbc:postgresql://localhost:5432/curso-jsp?autoReconnect=true";
+	private static String user = "postgres";
+	private static String password = "admin";
+	private static Connection connection = null;
+	
+	static {
+		
+		conectar();
+	}
+	
+	public SingleConnection() {
+		
+		conectar();
+	}
+	
+	private static void conectar() {
+		
+		try {
+			
+			if(connection == null) {
+				
+				Class.forName("org.postgresql.Driver"); // carrega o Driver do banco
+				connection = DriverManager.getConnection(url, user, password);
+				connection.setAutoCommit(false);
+				
+				
+			}
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();// mostra qualquer erro no momento da conexão com banco de dados
+		}
+	}
+
+	public static Connection getConnetion() {
+		
+		return connection;
+	}
 }
